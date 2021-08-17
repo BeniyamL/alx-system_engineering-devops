4882a72ee60f8ed2ffc449b6ccf72ddc58dbfd90
Beniyam Legesse <lbeniyam@gmail.com>
2021-08-17 06:43:05 -0700

    manage my server with puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,40 @@
+# configure the server with Puppet and manage it
+# Nginx should be listening on port 80
+# must return the page containing the string Holberton School
+# it must redirect to 301 moved permanently
+include stdlib
+
+$alx = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+$new_str = '\trewrite ^/redirect_me/$ $alx permanent;'
+
+exec {'restart':
+  command => 'usr/sbin/service nginx restart',
+  require => package['nginx']
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update']
+}
+
+exec {'update':
+  command  => 'usr/bin/apt-get update',
+}
+
+file { '/var/www/html/index.html':
+  ensure   => 'present',
+  content  => 'Holberton School',
+  mode     => '0644',
+  owner    => 'root',
+  group    => 'root'
+}
+
+file_line { '301 redirction':
+  ensure   => 'present',
+  after    => 'server_name _;',
+  path     => '/etc/nginx/sites-available/default',
+  multiple => true,
+  line     => $new_str,
+  notify   => Exec['restart'],
+  require  => File['/var/www/html/index.html']
+}
